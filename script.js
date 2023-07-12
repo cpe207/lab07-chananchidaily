d26bfa5b357b7bed355d01ecf6f4ee2925e5bf5d
tonkhao <chananchida_tha@cmu.ac.th>
2023-07-12 14:16:03 +0700

    okaaayy

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 //simple email validation
-function validateEmail(email) {
-  var atPos = email.indexOf("@");
-  var dotPos = email.lastIndexOf(".");
-  return atPos > 0 && dotPos > atPos + 1 && dotPos < email.length - 1;
+isFirstNameOk = false;
+isLastNameOk = false;
+isEmailOk = false;
+
+if (firstNameInput.value === "") {
+  firstNameInput.classList.add("is-valid");
+} else {
+  firstNameInput.classList.add("is-valid");
+  isFirstNameOk = true;
+}
+if (lastNameinput.value === "") {
+  lastNameinput.classList.add("is-invalid");
+} else {
+  lastNameinput.classList.add("is-valid");
+  isLastNameOk = true;
+}
+
+if (validateEmail(emailInput.value) === true) {
+  emailInput.classList.add("is-valid");
+  isEmailOk = true;
+} else {
+  emailInput.classList.add("is-invalid");
+}
+
+if (passwordinput.value.length >= 6) {
+  passwordinput.classList.add("is-valid");
+  isPasswordOk = true;
+} else {
+  passwordinput.classList.add("is-invalid");
+}
+if (isFirstNameOk && isLastNameOk && isEmailOk && isPasswordOk === true) {
+  alert("Registered successfully");
 }
